5fe6fcf0f4c1434c0e5b3b6d79a54bcd73adea4e
Priyanka1130 <55188287+Priyanka1130@users.noreply.github.com>
2022-02-10 17:52:22 +0530

    Delete two.py

--- two.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess   
-server_name=['localhost']
-for server in server_name:
-    response = os.popen(" ping {server_name}").read()
-    if "Recieved =1" in response:
-        print( "\u001b[32m  UP {server_name} ping successful")
-    else:
-        print("\u001b[31m DOWN {server_name} ping unsuccessful")
